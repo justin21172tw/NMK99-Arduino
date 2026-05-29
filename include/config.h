534daae025a7a066eb7c8e9316ce4f9c1b74feb8
justinhu <183563054+justin21172tw@users.noreply.github.com>
2026-05-29 18:39:33 +0800

    feat: configure the config.h based on wiring

--- include/config.h
+++ include/config.h
@@ -4,8 +4,11 @@
 #include <Arduino.h>
 
 namespace Config {
+namespace Features {
+constexpr bool ENABLE_LED = false;  // enable or disable LED control
+
 namespace Pins {
-constexpr uint8_t DHT11 = 4;
+constexpr uint8_t DHT11 = 13;
 constexpr uint8_t OLED_SDA = 21;
 constexpr uint8_t OLED_SCL = 22;
 constexpr uint8_t LED_RED = 25;
@@ -16,18 +19,18 @@ constexpr uint8_t LED_BLUE = 27;
 namespace Display {
 constexpr uint16_t WIDTH = 128;
 constexpr uint16_t HEIGHT = 64;
-constexpr uint8_t RESET_PIN = 255;
+constexpr uint8_t RESET_PIN = 255;  // whenever set to 225 or -1, it means not connected
 constexpr uint8_t PREFERRED_I2C_ADDRESS = 0x3C;
 }  // namespace Display
 
 namespace Dht {
-constexpr uint8_t TYPE = 11;
-constexpr uint32_t READ_INTERVAL_MS = 2000;
+constexpr uint8_t TYPE = 11; //which means DHT11
+constexpr uint32_t READ_INTERVAL_MS = 3000;  // read DHT11 every 3 seconds
 }  // namespace Dht
 
 namespace Humidity {
-constexpr float LOW_THRESHOLD = 40.0f;
-constexpr float HIGH_THRESHOLD = 70.0f;
+constexpr float LOW_THRESHOLD = 40.0f; // humudity lower then 40%
+constexpr float HIGH_THRESHOLD = 70.0f;  // humudity lower then 70%
 }  // namespace Humidity
 
 namespace Timing {
